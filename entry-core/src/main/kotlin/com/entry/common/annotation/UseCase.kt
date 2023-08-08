cb2618f7b595c8ceb099df9fd7fb664f0a66f654
HyunSu1768 <azxcv1768@gmail.com>
2023-08-08 11:19:26 +0900

    feat :: annotation

---
+++ entry-core/src/main/kotlin/com/entry/common/annotation/UseCase.kt
@@ -0,0 +1,8 @@
+package com.entry.common.annotation
+
+import org.springframework.transaction.annotation.Transactional
+
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS)
+@Transactional
+annotation class UseCase
