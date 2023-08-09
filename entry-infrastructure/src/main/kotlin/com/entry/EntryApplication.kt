1b72aef7e6d66612112c071b30255718b5b739a4
HyunSu1768 <azxcv1768@gmail.com>
2023-08-09 14:41:35 +0900

    fix bug

--- entry-infrastructure/src/main/kotlin/com/entry/EntryApplication.kt
+++ entry-infrastructure/src/main/kotlin/com/entry/EntryApplication.kt
@@ -1,14 +1,11 @@
 package com.entry
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
 class EntryApplication
 
-fun main(args: Array<String>){
+fun main(args: Array<String>) {
     runApplication<EntryApplication>(*args)
 }
