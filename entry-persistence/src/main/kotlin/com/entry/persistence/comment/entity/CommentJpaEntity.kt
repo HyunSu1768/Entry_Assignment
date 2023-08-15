1bf5b5fbbd1338624bcd1d880b3e3f1adde86bc0
HyunSu1768 <azxcv1768@gmail.com>
2023-08-15 17:58:56 +0900

    feat :: feat lazy 설정

--- entry-persistence/src/main/kotlin/com/entry/persistence/comment/entity/CommentJpaEntity.kt
+++ entry-persistence/src/main/kotlin/com/entry/persistence/comment/entity/CommentJpaEntity.kt
@@ -5,6 +5,7 @@ import com.entry.persistence.feed.entity.FeedJpaEntity
 import com.entry.persistence.user.entity.UserJpaEntity
 import java.util.UUID
 import javax.persistence.Entity
+import javax.persistence.FetchType
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
 
@@ -15,11 +16,11 @@ class CommentJpaEntity(
 
     val content: String,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     val user: UserJpaEntity,
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "feed_id")
     val feed: FeedJpaEntity
 
