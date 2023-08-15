9674a9f2d13a05f64080d35689bb53df2c0a4767
HyunSu1768 <azxcv1768@gmail.com>
2023-08-15 17:57:37 +0900

    refactor :: CustomUserDetails 분리

--- entry-core/src/main/kotlin/com/entry/user/model/User.kt
+++ entry-core/src/main/kotlin/com/entry/user/model/User.kt
@@ -1,41 +1,11 @@
 package com.entry.user.model
 
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
-import java.util.UUID
+import java.util.*
 
 data class User(
     val uuid: UUID? = UUID(0,0),
     val email: String,
     val nickname: String
-): UserDetails {
+) {
 
-    override fun getAuthorities(): MutableCollection<GrantedAuthority> {
-        return arrayListOf(SimpleGrantedAuthority("ROLE_TEST"))
-    }
-
-    override fun getPassword(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getUsername(): String {
-        return email
-    }
-
-    override fun isAccountNonExpired(): Boolean {
-        return false
-    }
-
-    override fun isAccountNonLocked(): Boolean {
-        return false
-    }
-
-    override fun isCredentialsNonExpired(): Boolean {
-        return false
-    }
-
-    override fun isEnabled(): Boolean {
-        return true
-    }
 }
--- entry-persistence/src/main/kotlin/com/entry/persistence/user/entity/UserJpaEntity.kt
+++ entry-persistence/src/main/kotlin/com/entry/persistence/user/entity/UserJpaEntity.kt
@@ -1,9 +1,6 @@
 package com.entry.persistence.user.entity
 
 import com.entry.persistence.BaseUUIDEntity
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.authority.SimpleGrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
 import java.util.*
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -18,34 +15,4 @@ class UserJpaEntity(
     @Column(name = "nickname", nullable = false)
     val nickname: String
 
-): BaseUUIDEntity(id), UserDetails {
-
-    override fun getAuthorities(): MutableCollection<GrantedAuthority> {
-        return arrayListOf(SimpleGrantedAuthority("ROLE_TEST"))
-    }
-
-    override fun getPassword(): String {
-        TODO("Not yet implemented")
-    }
-
-    override fun getUsername(): String {
-        return nickname
-    }
-
-    override fun isAccountNonExpired(): Boolean {
-        return false
-    }
-
-    override fun isAccountNonLocked(): Boolean {
-        return false
-    }
-
-    override fun isCredentialsNonExpired(): Boolean {
-        return false
-    }
-
-    override fun isEnabled(): Boolean {
-        return true
-
-    }
-}
+): BaseUUIDEntity(id)
