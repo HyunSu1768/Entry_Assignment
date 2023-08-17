99289a9468ac95c39fa96c01d3d6bf2a13620d52
HyunSu1768 <azxcv1768@gmail.com>
2023-08-17 15:23:03 +0900

    error code 추가

--- entry-core/src/main/kotlin/com/entry/common/error/ErrorCode.kt
+++ entry-core/src/main/kotlin/com/entry/common/error/ErrorCode.kt
@@ -6,13 +6,17 @@ enum class ErrorCode(
 ) {
 
     // UnAuthorized
-    EXPIRED_TOKEN(401, "Expired Token"),
-    INVALID_TOKEN(401, "Invalid Token"),
+    EXPIRED_TOKEN(401, "만료된 토큰입니다."),
+    INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
 
     // Interval Server Error
     INTERVAL_SERVER_ERROR(500, "Interval Server Error"),
 
     // 404 Not Found
-    FEED_NOT_FOUND(404, "Feed Not Found"),
-    STOCK_NOT_FOUND(404, "Stock Not Found")
+    FEED_NOT_FOUND(404, "찾을 수 없는 피드입니다."),
+    STOCK_NOT_FOUND(404, "찾을 수 없는 주식입니다."),
+    RECENT_FEED_NOT_FOUND(404, "최근 읽은 피드를 찾을 수 없습니다."),
+
+    // 403 Forbidden
+    WRITER_MISMATCH(403, "작성자만 수정할 수 있습니다.")
 }
