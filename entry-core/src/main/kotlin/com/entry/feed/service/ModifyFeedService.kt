cc9a5eea7800194a2f56cab7ec1b13e491ac4393
HyunSu1768 <azxcv1768@gmail.com>
2023-08-17 15:23:40 +0900

    예외처리

--- entry-core/src/main/kotlin/com/entry/feed/service/ModifyFeedService.kt
+++ entry-core/src/main/kotlin/com/entry/feed/service/ModifyFeedService.kt
@@ -1,21 +1,31 @@
 package com.entry.feed.service
 
 import com.entry.common.UseCase
+import com.entry.common.error.BusinessException
+import com.entry.common.error.ErrorCode
 import com.entry.feed.dto.request.FeedModifyRequest
 import com.entry.feed.port.`in`.ModifyFeedUseCase
 import com.entry.feed.port.out.LoadFeedByUUIDPort
 import com.entry.feed.port.out.SaveFeedPort
+import com.entry.user.port.out.LoadCurrentUserPort
 import java.util.*
 
 @UseCase
 class ModifyFeedService(
     private val loadFeedByUUIDPort: LoadFeedByUUIDPort,
-    private val saveFeedPort: SaveFeedPort
+    private val saveFeedPort: SaveFeedPort,
+    private val loadCurrentUserPort: LoadCurrentUserPort
     ): ModifyFeedUseCase{
 
     override fun modifyFeed(feedModifyRequest: FeedModifyRequest, id: UUID) {
 
         val feed = loadFeedByUUIDPort.loadFeed(id)
+
+        val user = loadCurrentUserPort.loadCurrentUser()
+
+        if(feed.user!=user){
+            throw BusinessException(ErrorCode.WRITER_MISMATCH)
+        }
 
         saveFeedPort.saveFeed(
             feed.copy(
